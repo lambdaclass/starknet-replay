f27cf3a34839e7940376cd4761a3405d1877b7d3
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-07-10 18:39:37 -0300

    Make code prettier

--- replay/src/benchmark.rs
+++ replay/src/benchmark.rs
@@ -1,4 +1,4 @@
-use crate::{build_cached_state, get_transaction_hashes, parse_network};
+use crate::{build_cached_state, get_transaction_hashes};
 use blockifier::{
     blockifier::block::BlockInfo,
     context::{BlockContext, ChainInfo},
@@ -13,17 +13,25 @@ use starknet_api::{
 };
 use tracing::{error, info};
 
+pub type BlockCachedData = (
+    CachedState<RpcStateReader>,
+    BlockContext,
+    Vec<(TransactionHash, SNTransaction)>,
+);
+
+/// Fetches context data to execute the given block range
+///
+/// It does not actually execute them, so not all required data will be cached.
+/// To ensure that all rpc data is cached, the block range must be execute
+/// at least once.
+///
+/// See `execute_block_range`
 pub fn fetch_block_range_data(
     block_start: BlockNumber,
     block_end: BlockNumber,
     chain: &str,
-) -> Vec<(
-    CachedState<RpcStateReader>,
-    BlockContext,
-    Vec<(TransactionHash, SNTransaction)>,
-)> {
+) -> Vec<BlockCachedData> {
     let mut block_caches = Vec::new();
-    let rpc_chain = parse_network(chain);
 
     for block_number in block_start.0..=block_end.0 {
         // For each block
@@ -33,26 +41,7 @@ pub fn fetch_block_range_data(
         let state = build_cached_state(&chain, block_number.0 - 1);
 
         // Fetch block context
-        let block_context = {
-            let rpc_block_info = state.state.0.get_block_info().unwrap();
-
-            let gas_price = state.state.0.get_gas_price(block_number.0).unwrap();
-
-            BlockContext::new_unchecked(
-                &BlockInfo {
-                    block_number,
-                    block_timestamp: rpc_block_info.block_timestamp,
-                    sequencer_address: rpc_block_info.sequencer_address,
-                    gas_prices: gas_price,
-                    use_kzg_da: false,
-                },
-                &ChainInfo {
-                    chain_id: rpc_chain.into(),
-                    fee_token_addresses: Default::default(),
-                },
-                &VersionedConstants::latest_constants_with_overrides(u32::MAX, usize::MAX),
-            )
-        };
+        let block_context = fetch_block_context(&state, block_number);
 
         // Fetch transactions for the block
         let transactions = get_transaction_hashes(&chain, block_number.0)
@@ -76,13 +65,33 @@ pub fn fetch_block_range_data(
     block_caches
 }
 
-pub fn execute_block_range(
-    block_range_data: &mut Vec<(
-        CachedState<RpcStateReader>,
-        BlockContext,
-        Vec<(TransactionHash, SNTransaction)>,
-    )>,
-) {
+fn fetch_block_context(
+    state: &CachedState<RpcStateReader>,
+    block_number: BlockNumber,
+) -> BlockContext {
+    let rpc_block_info = state.state.0.get_block_info().unwrap();
+    let gas_price = state.state.0.get_gas_price(block_number.0).unwrap();
+
+    BlockContext::new_unchecked(
+        &BlockInfo {
+            block_number,
+            block_timestamp: rpc_block_info.block_timestamp,
+            sequencer_address: rpc_block_info.sequencer_address,
+            gas_prices: gas_price,
+            use_kzg_da: false,
+        },
+        &ChainInfo {
+            chain_id: state.state.0.get_chain_name().into(),
+            fee_token_addresses: Default::default(),
+        },
+        &VersionedConstants::latest_constants_with_overrides(u32::MAX, usize::MAX),
+    )
+}
+
+/// Executes the given block range and discards any state changes
+///
+/// Can be used to fill up the cache
+pub fn execute_block_range(block_range_data: &mut Vec<BlockCachedData>) {
     for (state, block_context, transactions) in block_range_data {
         // The transactional state is used to execute a transaction while discarding all writes to it.
         let mut transactional_state = CachedState::create_transactional(state);
