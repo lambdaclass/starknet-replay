c443661045f86390c7f761e0f41890b1b1338369
Edgar Luque <git@edgarluque.com>
2024-09-10 14:16:17 +0200

    contract executor

--- rpc-state-reader/src/utils.rs
+++ rpc-state-reader/src/utils.rs
@@ -1,18 +1,14 @@
 use std::{
     collections::HashMap,
     io::{self, Read},
+    path::PathBuf,
     sync::{Arc, OnceLock, RwLock},
 };
 
 use cairo_lang_sierra::program::Program;
 use cairo_lang_starknet_classes::contract_class::ContractEntryPoints;
 use cairo_lang_utils::bigint::BigUintAsHex;
-use cairo_native::{
-    cache::{AotProgramCache, ProgramCache},
-    context::NativeContext,
-    executor::AotNativeExecutor,
-    OptLevel,
-};
+use cairo_native::{executor::contract::ContractExecutor, OptLevel};
 use serde::Deserialize;
 use starknet::core::types::{LegacyContractEntryPoint, LegacyEntryPointsByType};
 use starknet_api::{
@@ -29,10 +25,8 @@ pub struct MiddleSierraContractClass {
     pub entry_points_by_type: ContractEntryPoints,
 }
 
-static NATIVE_CONTEXT: std::sync::OnceLock<cairo_native::context::NativeContext> =
-    std::sync::OnceLock::new();
-
-static AOT_PROGRAM_CACHE: OnceLock<RwLock<ProgramCache<'static, ClassHash>>> = OnceLock::new();
+static AOT_PROGRAM_CACHE: OnceLock<RwLock<HashMap<ClassHash, Arc<ContractExecutor>>>> =
+    OnceLock::new();
 
 pub fn map_entry_points_by_type_legacy(
     entry_points_by_type: LegacyEntryPointsByType,
@@ -133,24 +127,39 @@ pub fn deserialize_transaction_json(
     }
 }
 
-pub fn get_native_executor(program: Program, class_hash: ClassHash) -> Arc<AotNativeExecutor> {
-    let program_cache = AOT_PROGRAM_CACHE.get_or_init(|| {
-        RwLock::new(ProgramCache::Aot(AotProgramCache::new(
-            NATIVE_CONTEXT.get_or_init(NativeContext::new),
-        )))
-    });
-    let native_executor = match &*program_cache.read().unwrap() {
-        ProgramCache::Aot(program_cache) => program_cache.get(&class_hash),
-        _ => panic!("JIT is not supported"),
-    };
+pub fn get_native_executor(program: Program, class_hash: ClassHash) -> Arc<ContractExecutor> {
+    let program_cache = AOT_PROGRAM_CACHE.get_or_init(|| RwLock::new(HashMap::with_capacity(32)));
+
+    let cache = program_cache.read().unwrap();
+    let native_executor = cache.get(&class_hash);
 
     match native_executor {
-        Some(native_executor) => native_executor,
-        None => match &mut *program_cache.write().unwrap() {
-            ProgramCache::Aot(program_cache) => {
-                program_cache.compile_and_insert(class_hash, &program, OptLevel::Default)
-            }
-            _ => panic!("JIT is not supported"),
-        },
+        Some(native_executor) => Arc::clone(native_executor),
+        None => {
+            let mut cache = program_cache.write().unwrap();
+
+            let path = PathBuf::from(format!(
+                "compiled_programs/{}.{}",
+                class_hash.to_hex_string(),
+                {
+                    if cfg!(target_os = "macos") {
+                        "dylib"
+                    } else {
+                        "so"
+                    }
+                }
+            ));
+
+            let executor = Arc::new(if path.exists() {
+                ContractExecutor::load(&path).unwrap()
+            } else {
+                let ex = ContractExecutor::new(&program, OptLevel::Default).unwrap();
+                ex.save(&path).unwrap();
+                ex
+            });
+
+            cache.insert(class_hash, Arc::clone(&executor));
+            executor
+        }
     }
 }
