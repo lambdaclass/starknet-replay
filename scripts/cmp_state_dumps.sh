f5a0516ea5299018c12a38c1545d59600978510f
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-09-24 10:59:26 -0300

    Move good dumps to another directory

--- scripts/cmp_state_dumps.sh
+++ scripts/cmp_state_dumps.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+mkdir -p good_state_dump/native
+mkdir -p good_state_dump/vm
+
 for native_dump in state_dump/native/*; do
   base_dump=$(basename -- "$native_dump")
   vm_dump="state_dump/vm/$base_dump"
 
   if ! cmp -s "$vm_dump" "$native_dump" ; then
     echo "diff at $base_dump"
+  else
+    echo "match at $base_dump"
+    mv "$native_dump" good_state_dump/native
+    mv "$vm_dump" good_state_dump/vm
   fi
 done
