da033721c48cb68bcaf0940380a1ac64c07228e4
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-09-26 18:37:54 -0300

    Update scripts

--- scripts/delta_state_dump.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-delta state_dump/{vm,native}/"$1"
--- scripts/delta_state_dumps.sh
+++ scripts/delta_state_dumps.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash
 
-for native_dump in state_dump/native/*; do
-  base_dump=$(basename -- "$native_dump")
-  vm_dump="state_dump/vm/$base_dump"
+# If there are a lot of transactions, it can be difficult to quit the script. This prompts if the user wants to continue after each diff.
+function prompt_continue {
+    while true; do
+      read -rp "Continue [Y/n]? " yn
+      case $yn in
+          [Yy]* ) break;;
+          [Nn]* ) exit;;
+          * ) break;;
+      esac
+  done
+}
 
-  if ! cmp -s "$vm_dump" "$native_dump" ; then
-    delta --paging always "$vm_dump" "$native_dump"
-  fi
+for block_dir in state_dumps/vm/*/; do
+  [ -d "$block_dir" ] || continue
+  block_base=$(basename "$block_dir")
+
+  echo "Block ${block_base//block/}"
+
+  for vm_dump in "$block_dir"/*.json; do
+    [ -f "$vm_dump" ] || continue
+    native_dump="${vm_dump//vm/native}"
+    if cmp -s "$native_dump" "$vm_dump" ; then
+      continue
+    fi
+
+    base=$(basename "$vm_dump")
+    echo "Tx ${base//.json/}"
+
+    prompt_continue
+
+    delta "$native_dump" "$vm_dump" --side-by-side --paging always --wrap-max-lines unlimited 
+  done
 done
