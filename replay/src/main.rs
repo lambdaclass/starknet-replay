c83bf8e1b79ad3f7d38022193f58004794411138
Edgar Luque <git@edgarluque.com>
2024-11-13 12:01:20 +0100

    fix

--- replay/src/main.rs
+++ replay/src/main.rs
@@ -298,7 +298,7 @@ fn show_execution_data(
         let root = Path::new("state_dumps/native");
         let root = root.join(format!("block{}", block_number));
 
-        std::fs::create_dir_all(root).ok();
+        std::fs::create_dir_all(&root).ok();
 
         let mut path = root.join(&tx_hash);
         path.set_extension("json");
