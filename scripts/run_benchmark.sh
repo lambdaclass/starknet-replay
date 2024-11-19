a526dbf8a52953b134c86ccfabb8551ef7b8191f
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-11-19 15:51:34 -0300

    Updaete script

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -5,7 +5,7 @@ END=874009
 LAPS=100
 
 echo "Running Native benchmark, to 'native-logs'"
-cargo run --release --features benchmark,structured_logging bench-block-range $START $END mainnet $LAPS > native-logs
+cargo run --release --features benchmark,structured_logging bench-block-range $START $END mainnet $LAPS | tee native-logs
 
 echo "Running VM benchmark, to 'vm-logs'"
-cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-block-range $START $END mainnet $LAPS > vm-logs
+cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-block-range $START $END mainnet $LAPS | tee vm-logs
