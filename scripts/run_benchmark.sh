757533d58100747e7bb919e1a5f9cdce9db06b9c
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-11-20 16:08:34 -0300

    Remove prints

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -7,8 +7,6 @@ LAPS=100
 
 output="block-$START-$END-$NET.jsonl"
 
-echo "Running Native benchmark"
 cargo run --release --features benchmark,structured_logging bench-block-range $START $END $NET $LAPS | tee "native-$output"
 
-echo "Running VM benchmark"
 cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-block-range $START $END $NET $LAPS | tee "vm-$output"
