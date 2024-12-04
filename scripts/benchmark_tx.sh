a83d791c9b3776f291771d810882a841f371bdd2
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-12-04 12:41:30 -0300

    Improve benchmark_tx script

--- scripts/benchmark_tx.sh
+++ scripts/benchmark_tx.sh
@@ -1,12 +1,42 @@
 #!/usr/bin/env bash
 
-TX=0x01e06dfbd41e559ee5edd313ab95605331873a5aed09bf1c7312456b7aa2a1c7
-BLOCK=291652
-LAPS=10000
-NET=testnet
+usage() {
+cat <<EOF
+Usage: $0 <tx> <net> <block> <laps>
 
-output="tx-$TX-$NET.jsonl"
+Benches a single transaction:
+- Saves output to native-<tx>-<net> and vm-<tx>-<net>
+- Prints speedup
+EOF
+}
 
-cargo run --release --features benchmark,structured_logging bench-tx $TX $NET $BLOCK $LAPS | tee "native-$output"
+if ! [ "$#" -ge "4" ]; then
+    usage
+    exit 1
+fi
 
-cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-tx $TX $NET $BLOCK $LAPS | tee "vm-$output"
+TX=$1
+NET=$2
+BLOCK=$3
+LAPS=$4
+
+echo "Benchmarking $NET $TX"
+
+output="$TX-$NET.jsonl"
+native_output="native-$output"
+vm_output="vm-$output"
+
+echo "Executing with Native"
+cargo run --release --features benchmark,structured_logging bench-tx "$TX" "$NET" "$BLOCK" "$LAPS" > "$native_output" 2>/dev/null
+
+native_time=$(tail -n1 "$native_output" | jq .fields.average_run_time)
+echo "Average Native time: $native_time"
+
+echo "Executing with VM"
+cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-tx "$TX" "$NET" "$BLOCK" "$LAPS" > "$vm_output" 2>/dev/null
+
+vm_time=$(tail -n1 "$vm_output" | jq .fields.average_run_time)
+echo "Average VM time: $vm_time"
+
+speedup=$(bc -le "$vm_time/$native_time")
+echo "Native Speedup: $speedup"
