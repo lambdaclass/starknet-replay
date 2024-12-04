1f5bcb416fd0de2d5a8dd4e2ed107943c0dec469
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-12-04 14:59:28 -0300

    Refactor block script

--- scripts/benchmark_block.sh
+++ scripts/benchmark_block.sh
@@ -1,12 +1,41 @@
 #!/usr/bin/env bash
 
-START=874000
-END=874009
-NET=mainnet
-LAPS=100
+usage() {
+cat <<EOF
+Usage: $0 <start> <end> <net> <laps>
+
+Benches a block range
+- Saves output to native-<start>-<end>-<net> and vm-<start>-<end>-<net>
+- Prints speedup
+EOF
+}
+
+if ! [ "$#" -ge "4" ]; then
+    usage
+    exit 1
+fi
+
+
+START=$1
+END=$2
+NET=$3
+LAPS=$4
 
 output="block-$START-$END-$NET.jsonl"
+native_output="native-$output"
+vm_output="vm-$output"
 
-cargo run --release --features benchmark,structured_logging bench-block-range $START $END $NET $LAPS | tee "native-$output"
+echo "Executing with Native"
+cargo run --release --features benchmark,structured_logging bench-block-range "$START" "$END" "$NET" "$LAPS" > "$native_output"
 
-cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-block-range $START $END $NET $LAPS | tee "vm-$output"
+native_time=$(tail -n1 "$native_output" | jq .fields.average_run_time)
+echo "Average Native time: $native_time"
+
+echo "Executing with VM"
+cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-block-range "$START" "$END" "$NET" "$LAPS" > "$vm_output"
+
+vm_time=$(tail -n1 "$vm_output" | jq .fields.average_run_time)
+echo "Average VM time: $vm_time"
+
+speedup=$(bc -l <<< "$vm_time/$native_time")
+echo "Native Speedup: $speedup"
