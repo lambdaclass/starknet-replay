e484f45cd4349b241cda481ba70805a59b830e23
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-12-05 15:33:35 -0300

    Update benchmark block

--- scripts/benchmark_block.sh
+++ scripts/benchmark_block.sh
@@ -21,20 +21,24 @@ END=$2
 NET=$3
 LAPS=$4
 
-output="block-$START-$END-$NET.jsonl"
-native_output="native-$output"
-vm_output="vm-$output"
+log_output="logs-$START-$END-$NET.jsonl"
+native_log_output="native-$log_output"
+vm_log_output="vm-$log_output"
+
+data_output="data-$START-$END-$NET.jsonl"
+native_data_output="native-$data_output"
+vm_data_output="vm-$data_output"
 
 echo "Executing with Native"
-cargo run --release --features benchmark,structured_logging bench-block-range "$START" "$END" "$NET" "$LAPS" > "$native_output"
+cargo run --release --features benchmark,structured_logging bench-block-range "$START" "$END" "$NET" "$LAPS" -o "$native_data_output" > "$native_log_output"
 
-native_time=$(tail -n1 "$native_output" | jq .fields.average_run_time)
+native_time=$(tail -n1 "$native_log_output" | jq .fields.average_run_time)
 echo "Average Native time: $native_time"
 
 echo "Executing with VM"
-cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-block-range "$START" "$END" "$NET" "$LAPS" > "$vm_output"
+cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-block-range "$START" "$END" "$NET" "$LAPS" -o "$vm_data_output" > "$vm_log_output"
 
-vm_time=$(tail -n1 "$vm_output" | jq .fields.average_run_time)
+vm_time=$(tail -n1 "$vm_log_output" | jq .fields.average_run_time)
 echo "Average VM time: $vm_time"
 
 speedup=$(bc -l <<< "$vm_time/$native_time")
