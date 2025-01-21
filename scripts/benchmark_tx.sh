dcad246aed51609fbdc32dad5b4509ffd058ace3
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-01-21 14:17:14 -0300

    Update scripts

--- scripts/benchmark_tx.sh
+++ scripts/benchmark_tx.sh
@@ -15,30 +15,50 @@ if ! [ "$#" -ge "4" ]; then
     exit 1
 fi
 
+NATIVE_TARGET=target/release/replay-bench-native
+VM_TARGET=target/release/replay-bench-vm
+
+if [ ! -x $NATIVE_TARGET ] || [ ! -x $VM_TARGET ]; then
+    echo "benchmark target is missing, please run: make deps-bench"
+    exit 1
+fi
+
 TX=$1
 NET=$2
 BLOCK=$3
 LAPS=$4
 
+DATA_DIR="bench_data"
+mkdir -p $DATA_DIR
+
 log_output="logs-$TX-$NET.jsonl"
-native_log_output="native-$log_output"
-vm_log_output="vm-$log_output"
+native_log_output="$DATA_DIR/native-$log_output"
+vm_log_output="$DATA_DIR/vm-$log_output"
 
 data_output="data-$TX-$NET.json"
-native_data_output="native-$data_output"
-vm_data_output="vm-$data_output"
+native_data_output="$DATA_DIR/native-$data_output"
+vm_data_output="$DATA_DIR/vm-$data_output"
+
+echo "Benchmarking $LAPS times $NET transaction $TX"
+echo
 
 echo "Executing with Native"
-cargo run --release --features benchmark,structured_logging bench-tx "$TX" "$NET" "$BLOCK" "$LAPS" -o "$native_data_output" > "$native_log_output"
+$NATIVE_TARGET bench-tx "$TX" "$NET" "$BLOCK" "$LAPS" -o "$native_data_output" > "$native_log_output"
 
-native_time=$(tail -n1 "$native_log_output" | jq .fields.average_run_time)
-echo "Average Native time: $native_time"
+native_time_secs=$(jq .average_time.secs "$native_data_output")
+native_time_nanos=$(jq .average_time.nanos "$native_data_output")
+native_time=$(bc -l <<< "$native_time_secs * 1e9 + $native_time_nanos")
+echo "Average Native time: $native_time ns"
+echo
 
 echo "Executing with VM"
-cargo run --release --features benchmark,structured_logging,only_cairo_vm bench-tx "$TX" "$NET" "$BLOCK" "$LAPS" -o "$vm_data_output" > "$vm_log_output"
+$VM_TARGET bench-tx "$TX" "$NET" "$BLOCK" "$LAPS" -o "$vm_data_output" > "$vm_log_output"
 
-vm_time=$(tail -n1 "$vm_log_output" | jq .fields.average_run_time)
-echo "Average VM time: $vm_time"
+vm_time_secs=$(jq .average_time.secs "$vm_data_output")
+vm_time_nanos=$(jq .average_time.nanos "$vm_data_output")
+vm_time=$(bc -l <<< "$vm_time_secs * 1e9 + $vm_time_nanos")
+echo "Average VM time: $vm_time ns"
+echo
 
 speedup=$(bc -l <<< "$vm_time/$native_time")
 echo "Native Speedup: $speedup"
