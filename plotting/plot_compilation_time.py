e6ee0bf618d44329b1558ba73cffb43698047619
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-01-22 18:36:53 -0300

    Refactor plot_compilation_time

--- plotting/plot_compilation_time.py
+++ plotting/plot_compilation_time.py
@@ -1,47 +1,37 @@
 from argparse import ArgumentParser
+import matplotlib.pyplot as plt
+import seaborn as sns
+from utils import load_log, find_span
 
-argument_parser = ArgumentParser('Stress Test Plotter')
-argument_parser.add_argument("native_logs_path")
+
+argument_parser = ArgumentParser("Stress Test Plotter")
+argument_parser.add_argument("logs_path")
 arguments = argument_parser.parse_args()
 
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
 
-dataset = pd.read_json(arguments.native_logs_path, lines=True, typ="series")
-
-def canonicalize_compilation_time(event):
+def canonicalize(event):
     # keep contract compilation finished logs
-    if "contract compilation finished" not in event["fields"]["message"]:
+    if "native contract compilation finished" not in event["fields"]["message"]:
         return None
 
     compilation_span = find_span(event, "contract compilation")
     if compilation_span is None:
         return None
-    
+
     return {
         "class hash": compilation_span["class_hash"],
         "time": float(event["fields"]["time"]),
     }
 
-def find_span(event, name):
-    for span in event["spans"]:
-        if name in span["name"]:
-            return span
-    return None
 
-def format_hash(class_hash):
-    return f"0x{class_hash[:6]}..."
-
-dataset = dataset.apply(canonicalize_compilation_time).dropna().apply(pd.Series)
+dataset = load_log(arguments.logs_path, canonicalize)
 
 figure, ax = plt.subplots()
 
 sns.set_color_codes("bright")
-sns.barplot(ax=ax, y="class hash", x="time", data=dataset, formatter=format_hash) # type: ignore
+sns.violinplot(ax=ax, x="time", data=dataset, cut=0)
 
 ax.set_xlabel("Compilation Time (ms)")
-ax.set_ylabel("Class Hash")
 ax.set_title("Native Compilation Time")
 
 plt.show()
