d967e1e860a13e9991c812525f82d8e5782fcb23
FrancoGiachetta <francogiachetta27@gmail.com>
2025-02-05 16:16:43 -0300

    format

--- rpc-state-reader/src/utils.rs
+++ rpc-state-reader/src/utils.rs
@@ -112,7 +112,7 @@ pub fn get_native_executor(contract: &ContractClass, class_hash: ClassHash) -> A
                 let pre_compilation_instant = Instant::now();
                 let (sierra_version, _) =
                     version_id_from_serialized_sierra_program(&contract.sierra_program).unwrap();
-                
+
                 loop {
                     // it could be the case that the file was created after we've entered this branch
                     // so we should load it instead of compiling it again
