6d03e4ccb89bb1f7765dab245204107fe44f3b9d
FrancoGiachetta <francogiachetta27@gmail.com>
2025-02-05 17:30:15 -0300

    clippy

--- rpc-state-reader/src/utils.rs
+++ rpc-state-reader/src/utils.rs
@@ -162,7 +162,7 @@ pub fn get_native_executor(contract: &ContractClass, class_hash: ClassHash) -> A
                         }
                     }
                 }
-            };
+            }
         }
     }
 }
