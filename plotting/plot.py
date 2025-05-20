96fd1b904819cfc71f51765dacbb7956dd4c9e86
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-05-20 14:03:20 -0300

    Reorder plots

--- plotting/plot.py
+++ plotting/plot.py
@@ -30,44 +30,6 @@ for stat_file in stat_files:
 
 df = pd.DataFrame(stats).set_index("hash")
 
-if "Correlations Matrix" in enabled_plots:
-    fig, ax = plt.subplots()
-    fig.suptitle("Correlations Matrix")
-
-    df_corr = df.corr(numeric_only=True)
-    sns.heatmap(df_corr, ax=ax)
-
-if "Individual Correlations" in enabled_plots:
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-    fig.suptitle("Individual Correlations")
-
-    # HIGH CORRELATION BETWEEN
-    # - sierra_statement_count
-    # - mlir_operation_count
-    # - llvmir_instruction_count
-    # - llvmir_virtual_register_count
-    sns.regplot(df, ax=ax1, x="sierra_statement_count", y="mlir_operation_count")
-    ax1.set_title("Sierra Size vs. MLIR Size")
-
-    sns.regplot(
-        df, ax=ax2, x="sierra_statement_count", y="llvmir_virtual_register_count"
-    )
-    ax2.set_title("Sierra Size vs. LLVM Virtual Registers")
-
-    # HIGH CORRELATION BETWEEN
-    # - compilation_total_time_ms
-    # - compilation_llvm_passes_time_ms
-    # - compilation_llvm_to_object_time_ms
-    # - object_size_bytes
-    sns.regplot(df, ax=ax3, x="compilation_total_time_ms", y="object_size_bytes")
-    ax3.set_title("Compilation Time vs. Object Size")
-
-    # LOW CORRELATION BETWEEN BOTH GROUPS
-    # - sierra_statement_count
-    # - compilation_total_time_ms
-    sns.regplot(df, ax=ax4, x="sierra_statement_count", y="compilation_total_time_ms")
-    ax4.set_title("Sierra Size vs. Compilation Time")
-
 
 def group_small_entries(entries, cutoff):
     new_entries = {}
@@ -108,10 +70,12 @@ def plot_pie(light_contract, heavy_contract, attribute, title):
 heavy_contract = df.loc[SAMPLE_HEAVY_CONTRACT]
 light_contract = df.loc[SAMPLE_LIGHT_CONTRACT]
 
-
-if "Sierra Libfunc Pie" in enabled_plots:
+if "MLIR by Libfunc Pie" in enabled_plots:
     plot_pie(
-        light_contract, heavy_contract, "sierra_libfunc_frequency", "Sierra Libfunc Pie"
+        light_contract,
+        heavy_contract,
+        "mlir_operations_by_libfunc",
+        "MLIR by Libfunc Pie",
     )
 
 if "LLVM Instruction Pie" in enabled_plots:
@@ -122,12 +86,48 @@ if "LLVM Instruction Pie" in enabled_plots:
         "LLVM Instruction Pie",
     )
 
-if "MLIR by Libfunc Pie" in enabled_plots:
+
+if "Sierra Libfunc Pie" in enabled_plots:
     plot_pie(
-        light_contract,
-        heavy_contract,
-        "mlir_operations_by_libfunc",
-        "MLIR by Libfunc Pie",
+        light_contract, heavy_contract, "sierra_libfunc_frequency", "Sierra Libfunc Pie"
     )
+
+if "Individual Correlations" in enabled_plots:
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    fig.suptitle("Individual Correlations")
+
+    # HIGH CORRELATION BETWEEN
+    # - sierra_statement_count
+    # - mlir_operation_count
+    # - llvmir_instruction_count
+    # - llvmir_virtual_register_count
+    sns.regplot(df, ax=ax1, x="sierra_statement_count", y="mlir_operation_count")
+    ax1.set_title("Sierra Size vs. MLIR Size")
+
+    sns.regplot(
+        df, ax=ax2, x="sierra_statement_count", y="llvmir_virtual_register_count"
+    )
+    ax2.set_title("Sierra Size vs. LLVM Virtual Registers")
+
+    # HIGH CORRELATION BETWEEN
+    # - compilation_total_time_ms
+    # - compilation_llvm_passes_time_ms
+    # - compilation_llvm_to_object_time_ms
+    # - object_size_bytes
+    sns.regplot(df, ax=ax3, x="compilation_total_time_ms", y="object_size_bytes")
+    ax3.set_title("Compilation Time vs. Object Size")
+
+    # LOW CORRELATION BETWEEN BOTH GROUPS
+    # - sierra_statement_count
+    # - compilation_total_time_ms
+    sns.regplot(df, ax=ax4, x="sierra_statement_count", y="compilation_total_time_ms")
+    ax4.set_title("Sierra Size vs. Compilation Time")
+
+if "Correlations Matrix" in enabled_plots:
+    fig, ax = plt.subplots()
+    fig.suptitle("Correlations Matrix")
+
+    df_corr = df.corr(numeric_only=True)
+    sns.heatmap(df_corr, ax=ax)
 
 plt.show()
