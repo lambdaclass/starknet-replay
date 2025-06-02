bbfc36698d3d54a34d699e701b2f0f1b1aece241
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-06-02 14:59:07 -0300

    Document cairo-to-contract

--- tools/src/bin/cairo-to-contract.rs
+++ tools/src/bin/cairo-to-contract.rs
@@ -1,16 +1,22 @@
-use cairo_lang_compiler::{CompilerConfig, diagnostics::DiagnosticsReporter};
+use cairo_lang_compiler::{diagnostics::DiagnosticsReporter, CompilerConfig};
 use cairo_lang_starknet::compile::starknet_compile;
+use clap::{command, Parser};
+
+/// Compiles a Cairo contract
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // Path to cairo file
+    cairo_path: String,
+}
 
 fn main() {
-    let mut args = std::env::args();
-    args.next();
-
-    let cairo_path = args.next().expect("expected cairo path as first argument");
+    let args = Args::parse();
 
     let diagnostics_reporter = DiagnosticsReporter::stderr().allow_warnings();
 
     let contract = starknet_compile(
-        cairo_path.into(),
+        args.cairo_path.into(),
         None,
         Some(CompilerConfig {
             replace_ids: true,
