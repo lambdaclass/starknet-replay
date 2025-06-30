ea5f5d289a08db1362670d3cf2fe656e9c80f9d2
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-06-30 17:30:44 -0300

    Add some getters to state manager

--- state-reader/src/lib.rs
+++ state-reader/src/lib.rs
@@ -1,3 +1,6 @@
+use std::io::{self, Read};
+
+use cairo_vm::types::program::Program;
 use remote_reader::RemoteReader;
 use starknet_api::{
     block::BlockNumber,
@@ -5,11 +8,15 @@ use starknet_api::{
     state::StorageKey,
 };
 
+use starknet_core::types::{CompressedLegacyContractClass, ContractClass, FlattenedSierraClass};
 use starknet_types_core::felt::Felt;
 
 use blockifier::{
     execution::contract_class::RunnableCompiledClass,
-    state::state_api::{StateReader, StateResult},
+    state::{
+        errors::StateError,
+        state_api::{StateReader, StateResult},
+    },
 };
 
 pub mod remote_reader;
@@ -27,22 +34,37 @@ impl StateReader for StateManager {
         contract_address: ContractAddress,
         key: StorageKey,
     ) -> StateResult<Felt> {
-        todo!()
+        let result = match self.remote_reader.get_storage_at(contract_address, key) {
+            Ok(x) => x,
+            Err(err) => return Err(err.into()),
+        };
+
+        Ok(result)
     }
 
     fn get_nonce_at(&self, contract_address: ContractAddress) -> StateResult<Nonce> {
-        todo!()
+        let result = match self.remote_reader.get_nonce_at(contract_address) {
+            Ok(x) => x,
+            Err(err) => return Err(err.into()),
+        };
+
+        Ok(result)
     }
 
     fn get_class_hash_at(&self, contract_address: ContractAddress) -> StateResult<ClassHash> {
-        todo!()
+        let result = match self.remote_reader.get_class_hash_at(contract_address) {
+            Ok(x) => x,
+            Err(err) => return Err(err.into()),
+        };
+
+        Ok(result)
     }
 
     fn get_compiled_class(&self, class_hash: ClassHash) -> StateResult<RunnableCompiledClass> {
-        todo!()
+        todo!();
     }
 
-    fn get_compiled_class_hash(&self, class_hash: ClassHash) -> StateResult<CompiledClassHash> {
-        todo!()
+    fn get_compiled_class_hash(&self, _: ClassHash) -> StateResult<CompiledClassHash> {
+        unimplemented!("compiled class hash is unused yet")
     }
 }
