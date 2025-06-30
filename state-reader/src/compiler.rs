f12e56a3266cd05674a361e2c121937d42756526
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-06-30 19:02:47 -0300

    Add basic state manager with compiling logic

---
+++ state-reader/src/compiler.rs
@@ -0,0 +1,237 @@
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::{self, Read},
+    path::PathBuf,
+    sync::Arc,
+    thread::sleep,
+    time::Duration,
+};
+
+use blockifier::execution::{
+    contract_class::{
+        CompiledClassV0, CompiledClassV0Inner, CompiledClassV1, RunnableCompiledClass,
+    },
+    native::{contract_class::NativeCompiledClassV1, executor::ContractExecutor},
+};
+use cairo_native::{executor::AotContractExecutor, statistics::Statistics, OptLevel};
+use cairo_vm::types::{errors::program_errors::ProgramError, program::Program};
+use starknet_api::{
+    contract_class::{EntryPointType, SierraVersion},
+    core::{ClassHash, EntryPointSelector},
+    deprecated_contract_class::{EntryPointOffset, EntryPointV0},
+    hash::StarkHash,
+};
+use starknet_core::types::{
+    CompressedLegacyContractClass, ContractClass as ProcessedContractClass, FlattenedSierraClass,
+    LegacyContractEntryPoint, LegacyEntryPointsByType,
+};
+use thiserror::Error;
+
+use cairo_lang_starknet_classes::{
+    casm_contract_class::StarknetSierraCompilationError,
+    contract_class::{version_id_from_serialized_sierra_program, ContractClass},
+};
+
+#[derive(Error, Debug)]
+pub enum ClassManagerError {
+    #[error("could not find compiled class in cache")]
+    CompiledClassNotFound,
+    #[error(transparent)]
+    CairoNativeError(#[from] cairo_native::error::Error),
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+    #[error(transparent)]
+    ProgramError(#[from] ProgramError),
+    #[error(transparent)]
+    SerdeJsonError(#[from] serde_json::Error),
+    #[error(transparent)]
+    StarknetApiError(#[from] starknet_api::StarknetApiError),
+    #[error(transparent)]
+    StarknetSierraCompilationError(#[from] StarknetSierraCompilationError),
+}
+
+pub fn compile_class(
+    class_hash: &ClassHash,
+    contract_class: ProcessedContractClass,
+) -> Result<RunnableCompiledClass, ClassManagerError> {
+    Ok(match contract_class {
+        ProcessedContractClass::Legacy(legacy_class) => compile_legacy_class(legacy_class)?,
+        ProcessedContractClass::Sierra(sierra_class) => {
+            let contract_class = processed_class_to_contract_class(sierra_class)?;
+            let casm_class = compile_contract_class(contract_class.clone())?;
+            let native_executor = compile_native_class(class_hash, contract_class)?;
+
+            let contract_executor = ContractExecutor::Aot(native_executor);
+
+            RunnableCompiledClass::V1Native(NativeCompiledClassV1::new(
+                contract_executor,
+                casm_class,
+            ))
+        }
+    })
+}
+
+fn compile_legacy_class(
+    legacy_class: CompressedLegacyContractClass,
+) -> Result<RunnableCompiledClass, ClassManagerError> {
+    let program_as_string = gz_decode_bytes_into_string(&legacy_class.program)?;
+    let program = Program::from_bytes(program_as_string.as_bytes(), None)?;
+    let entry_points_by_type = map_legacy_entrypoints_by_type(legacy_class.entry_points_by_type);
+    Ok(RunnableCompiledClass::V0(CompiledClassV0(Arc::new(
+        CompiledClassV0Inner {
+            program,
+            entry_points_by_type,
+        },
+    ))))
+}
+
+fn compile_contract_class(
+    contract_class: ContractClass,
+) -> Result<CompiledClassV1, ClassManagerError> {
+    let sierra_program_values = contract_class
+        .sierra_program
+        .iter()
+        .take(3)
+        .map(|felt| felt.value.clone())
+        .collect::<Vec<_>>();
+
+    let sierra_version = SierraVersion::extract_from_program(&sierra_program_values)?;
+
+    let casm_class =
+        cairo_lang_starknet_classes::casm_contract_class::CasmContractClass::from_contract_class(
+            contract_class,
+            false,
+            usize::MAX,
+        )?;
+
+    let versioned_casm = (casm_class, sierra_version);
+    Ok(CompiledClassV1::try_from(versioned_casm)?)
+}
+
+pub fn compile_native_class(
+    class_hash: &ClassHash,
+    contract_class: ContractClass,
+) -> Result<AotContractExecutor, ClassManagerError> {
+    let cache_path = PathBuf::from(format!(
+        "compiled_programs/{}.{}",
+        class_hash.to_hex_string(),
+        {
+            if cfg!(target_os = "macos") {
+                "dylib"
+            } else {
+                "so"
+            }
+        }
+    ));
+
+    if let Some(p) = cache_path.parent() {
+        fs::create_dir_all(p)?;
+    }
+
+    let (sierra_version, _) =
+        version_id_from_serialized_sierra_program(&contract_class.sierra_program)
+            .map_err(|err| StarknetSierraCompilationError::from(err))?;
+
+    let executor = loop {
+        // it could be the case that the file was created after we've entered this branch
+        // so we should load it instead of compiling it again
+        if cache_path.exists() {
+            match AotContractExecutor::from_path(&cache_path)? {
+                None => {
+                    sleep(Duration::from_secs(1));
+                    continue;
+                }
+                Some(e) => break e,
+            }
+        }
+
+        let mut statistics = if cfg!(feature = "with-comp-stats") {
+            Some(Statistics::default())
+        } else {
+            None
+        };
+
+        match AotContractExecutor::new_into(
+            &contract_class
+                .extract_sierra_program()
+                .map_err(|err| StarknetSierraCompilationError::from(err))?,
+            &contract_class.entry_points_by_type,
+            sierra_version,
+            &cache_path,
+            OptLevel::Aggressive,
+            statistics.as_mut(),
+        )? {
+            Some(e) => {
+                if let Some(statistics) = statistics {
+                    let stats_path = cache_path.with_extension("stats.json");
+                    let stats_file = File::create(stats_path)?;
+                    serde_json::to_writer_pretty(stats_file, &statistics)?;
+                }
+
+                break e;
+            }
+            None => {
+                sleep(Duration::from_secs(1));
+                continue;
+            }
+        }
+    };
+
+    Ok(executor)
+}
+
+/// Converts the processed class format into the compiler class format.
+pub fn processed_class_to_contract_class(
+    sierra_class: FlattenedSierraClass,
+) -> Result<ContractClass, ClassManagerError> {
+    let mut value = serde_json::to_value(sierra_class)?;
+    value
+        .as_object_mut()
+        .expect("should be an object")
+        .remove("abi");
+    Ok(serde_json::from_value(value)?)
+}
+
+/// Decodes a gz encoded byte slice, and returns a string. May fail if the byte
+/// slice is encoded incorrectly, or if the output is not a valid string.
+pub fn gz_decode_bytes_into_string(bytes: &[u8]) -> io::Result<String> {
+    use flate2::bufread;
+    let mut decoder = bufread::GzDecoder::new(&bytes[..]);
+    let mut string = String::new();
+    decoder.read_to_string(&mut string)?;
+    Ok(string)
+}
+
+/// Builds a map with legacy entrypoints by type.
+pub fn map_legacy_entrypoints_by_type(
+    entry_points_by_type: LegacyEntryPointsByType,
+) -> HashMap<EntryPointType, Vec<EntryPointV0>> {
+    let entry_types_to_points = HashMap::from([
+        (
+            EntryPointType::Constructor,
+            entry_points_by_type.constructor,
+        ),
+        (EntryPointType::External, entry_points_by_type.external),
+        (EntryPointType::L1Handler, entry_points_by_type.l1_handler),
+    ]);
+
+    let to_contract_entry_point = |entrypoint: &LegacyContractEntryPoint| -> EntryPointV0 {
+        let felt: StarkHash = StarkHash::from_bytes_be(&entrypoint.selector.to_bytes_be());
+        EntryPointV0 {
+            offset: EntryPointOffset(entrypoint.offset as usize),
+            selector: EntryPointSelector(felt),
+        }
+    };
+
+    let mut entry_points_by_type_map = HashMap::new();
+    for (entry_point_type, entry_points) in entry_types_to_points.into_iter() {
+        let values = entry_points
+            .iter()
+            .map(to_contract_entry_point)
+            .collect::<Vec<_>>();
+        entry_points_by_type_map.insert(entry_point_type, values);
+    }
+
+    entry_points_by_type_map
+}
--- state-reader/src/lib.rs
+++ state-reader/src/lib.rs
@@ -1,6 +1,4 @@
-use std::io::{self, Read};
-
-use cairo_vm::types::program::Program;
+use compiler::compile_class;
 use remote_reader::RemoteReader;
 use starknet_api::{
     block::BlockNumber,
@@ -8,7 +6,6 @@ use starknet_api::{
     state::StorageKey,
 };
 
-use starknet_core::types::{CompressedLegacyContractClass, ContractClass, FlattenedSierraClass};
 use starknet_types_core::felt::Felt;
 
 use blockifier::{
@@ -19,13 +16,21 @@ use blockifier::{
     },
 };
 
+pub mod compiler;
 pub mod remote_reader;
 
 pub struct StateManager {
-    with_cairo_native: bool,
     block_number: BlockNumber,
-
     remote_reader: RemoteReader,
+}
+
+impl StateManager {
+    pub fn new(block_number: BlockNumber, remote_reader: RemoteReader) -> Self {
+        Self {
+            block_number,
+            remote_reader,
+        }
+    }
 }
 
 impl StateReader for StateManager {
@@ -34,16 +39,23 @@ impl StateReader for StateManager {
         contract_address: ContractAddress,
         key: StorageKey,
     ) -> StateResult<Felt> {
-        let result = match self.remote_reader.get_storage_at(contract_address, key) {
-            Ok(x) => x,
-            Err(err) => return Err(err.into()),
-        };
+        let result =
+            match self
+                .remote_reader
+                .get_storage_at(self.block_number, contract_address, key)
+            {
+                Ok(x) => x,
+                Err(err) => return Err(err.into()),
+            };
 
         Ok(result)
     }
 
     fn get_nonce_at(&self, contract_address: ContractAddress) -> StateResult<Nonce> {
-        let result = match self.remote_reader.get_nonce_at(contract_address) {
+        let result = match self
+            .remote_reader
+            .get_nonce_at(self.block_number, contract_address)
+        {
             Ok(x) => x,
             Err(err) => return Err(err.into()),
         };
@@ -52,7 +64,10 @@ impl StateReader for StateManager {
     }
 
     fn get_class_hash_at(&self, contract_address: ContractAddress) -> StateResult<ClassHash> {
-        let result = match self.remote_reader.get_class_hash_at(contract_address) {
+        let result = match self
+            .remote_reader
+            .get_class_hash_at(self.block_number, contract_address)
+        {
             Ok(x) => x,
             Err(err) => return Err(err.into()),
         };
@@ -61,10 +76,72 @@ impl StateReader for StateManager {
     }
 
     fn get_compiled_class(&self, class_hash: ClassHash) -> StateResult<RunnableCompiledClass> {
-        todo!();
+        let contract_class = match self
+            .remote_reader
+            .get_contract_class(self.block_number, &class_hash)
+        {
+            Ok(x) => x,
+            Err(err) => return Err(err.into()),
+        };
+
+        let runnable_class = compile_class(&class_hash, contract_class)
+            .map_err(|err| StateError::StateReadError(err.to_string()))?;
+
+        Ok(runnable_class)
     }
 
     fn get_compiled_class_hash(&self, _: ClassHash) -> StateResult<CompiledClassHash> {
         unimplemented!("compiled class hash is unused yet")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use blockifier::state::state_api::StateReader;
+    use starknet_api::{block::BlockNumber, class_hash, core::ChainId};
+
+    use crate::{
+        remote_reader::{url_from_env, RemoteReader},
+        StateManager,
+    };
+
+    #[test]
+    pub fn get_contract_class() {
+        let url = url_from_env(ChainId::Mainnet);
+        let remote_reader = RemoteReader::new(url);
+
+        let state = StateManager::new(BlockNumber(1500000), remote_reader);
+
+        state
+            .get_compiled_class(class_hash!(
+                "0x07f3331378862ed0a10f8c3d49f4650eb845af48f1c8120591a43da8f6f12679"
+            ))
+            .unwrap();
+
+        state
+            .get_compiled_class(class_hash!(
+                "0x07f3331378862ed0a10f8c3d49f4650eb845af48f1c8120591a43da8f6f12679"
+            ))
+            .unwrap();
+    }
+
+    #[test]
+    pub fn get_legacy_contract_class() {
+        let url = url_from_env(ChainId::Mainnet);
+        let remote_reader = RemoteReader::new(url);
+
+        let state = StateManager::new(BlockNumber(1500000), remote_reader);
+
+        state
+            .get_compiled_class(class_hash!(
+                "0x010455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8"
+            ))
+            .unwrap();
+
+        state
+            .get_compiled_class(class_hash!(
+                "0x010455c752b86932ce552f2b0fe81a880746649b9aee7e0d842bf3f52378f9f8"
+            ))
+            .unwrap();
     }
 }
