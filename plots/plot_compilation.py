0d3f54703551a1fe2885976077b7fe73bd3dab7c
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-09-29 14:40:43 -0300

    Add plot_compilation.py script

---
+++ plots/plot_compilation.py
@@ -0,0 +1,43 @@
+import argparse
+import pathlib
+import inflection
+import json
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", type=pathlib.Path)
+parser.add_argument("output", type=pathlib.Path)
+args = parser.parse_args()
+
+
+def save_artifact(metadata):
+    slug = inflection.parameterize(metadata["title"])
+
+    plt.savefig(f"{slug}.pdf")
+    with open(f"{slug}.meta.json", "w") as f:
+        json.dump(metadata, f)
+
+
+nanosecond_to_second_formatter = matplotlib.ticker.FuncFormatter(
+    lambda x, _: f"{x / 1e9}"
+)
+
+df = pd.read_csv(args.input)
+print(df.info())
+
+_, ax = plt.subplots()
+sns.boxplot(df, ax=ax, x="native_time_ns", showfliers=False)
+ax.xaxis.set_major_formatter(nanosecond_to_second_formatter)
+ax.xaxis.set_label_text("Time (s)")
+ax.set_title("Compilation Time Distribution")
+save_artifact(
+    {
+        "title": "Compilation Time Distribution",
+    }
+)
+
+plt.show()
