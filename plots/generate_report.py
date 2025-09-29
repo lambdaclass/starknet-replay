4b2bae11d3b620236cf3c446c5c1b24682c0c249
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-09-29 16:54:46 -0300

    Use yattag

--- plots/generate_report.py
+++ plots/generate_report.py
@@ -1,7 +1,7 @@
 import argparse
 import pathlib
 import json
-import textwrap
+import yattag
 
 STYLESHEET = """
 body {
@@ -23,52 +23,52 @@ parser.add_argument("inputs", nargs="*", type=pathlib.Path)
 parser.add_argument("output", type=pathlib.Path)
 args = parser.parse_args()
 
-document = f"""\
-<html>
-<head><style>{STYLESHEET}</style></head>
-<body>
-<h1>Report</h1>
-"""
+doc, tag, text, line = yattag.Doc().ttl()
 
-for artifact_path in args.inputs:
-    artifact_path: pathlib.Path = artifact_path
-    metadata_path = artifact_path.with_suffix(".meta.json")
 
-    with open(f"{metadata_path}", "r") as f:
-        metadata = json.load(f)
+def add_head():
+    with tag("head"):
+        with tag("style"):
+            doc.asis(STYLESHEET)
 
-    document += textwrap.dedent(f"""
-        <h2>{metadata["title"]}</h2>
-    """)
 
-    if "description" in metadata:
-        document += textwrap.dedent(f"""
-            <p>{metadata["description"]}</p>
-        """)
+def add_body():
+    with tag("body"):
+        line("h1", "Report")
+        add_artifacts()
 
-    if "statistics" in metadata:
-        document += textwrap.dedent("""
-            <p><b>Statistics:</b></p>
-            <ul>
-        """)
-        for key, value in metadata["statistics"].items():
-            document += textwrap.dedent(f"""
-                <li><b>{key}:</b> {value}</li>
-            """)
-            pass
-        document += textwrap.dedent("""
-            </ul>
-        """)
 
-    if artifact_path.suffix == ".svg":
-        document += textwrap.dedent(f"""
-            <img src={artifact_path}></img>
-        """)
+def add_artifacts():
+    for artifact_path in args.inputs:
+        artifact_path: pathlib.Path = artifact_path
+        metadata_path = artifact_path.with_suffix(".meta.json")
 
-document += """\
-</body>
-</html>
-"""
+        with open(f"{metadata_path}", "r") as f:
+            metadata = json.load(f)
+
+        line("h2", metadata["title"])
+
+        if "description" in metadata:
+            line("p", metadata["description"])
+
+        if "statistics" in metadata:
+            with tag("p"):
+                line("b", "Statistics:")
+
+            with tag("ul"):
+                for key, value in metadata["statistics"].items():
+                    with tag("li"):
+                        line("b", f"{key}: ")
+                        text(value)
+
+        if artifact_path.suffix == ".svg":
+            doc.stag("img", src=str(artifact_path))
+
+
+doc.asis("<!DOCTYPE html>")
+with tag("html"):
+    add_head()
+    add_body()
 
 with open(args.output, "w") as f:
-    f.write(document)
+    f.write(yattag.indent(doc.getvalue()))
