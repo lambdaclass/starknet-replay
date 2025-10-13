f7521137299fcdacfde7064103bb174c6a4db827
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-10-13 17:55:51 -0300

    Add range subcommand

--- scripts/mux.sh
+++ scripts/mux.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 ENVRC=.envrc
 
-yell() { echo "$0:" "$@" >&2; }
+yell() { echo -e "$0:" "$@" >&2; }
 
 usage() {
 cat >&2 <<EOF
@@ -18,13 +18,65 @@ spawn() {
 	command="$2"
 
 	if tmux has-session -t "$name" 2>/dev/null; then
-		echo "session $name already exists"
+		echo "Session $name already exists"
 		return 1
 	else
 		tmux new-session -d -s "$name" "/bin/bash"
 		tmux send-keys -t "$name" "$command" C-m
 		return 0
 	fi
+}
+
+range() {
+	if [[ $# -lt 3 ]]; then
+		yell "range expects 3 positional argument\n"
+		usage
+	fi
+	START_BLOCK="$1"
+	RANGE_SIZE="$2"
+	N_WORKERS="$3"
+
+	step_size=$(((RANGE_SIZE + N_WORKERS - 1) / N_WORKERS))
+	end_block=$((START_BLOCK + RANGE_SIZE - 1))
+
+	echo "Building replay for Cairo Native"
+	cargo build -q --release --features state_dump 2>/dev/null
+	cp ./target/release/replay ./target/release/replay-native
+	echo "Building replay for Cairo VM"
+	cargo build -q --release --features state_dump,only_cairo_vm 2>/dev/null
+	cp ./target/release/replay ./target/release/replay-vm
+
+	for ((i = START_BLOCK ; i <= end_block ; i += step_size )); do
+		current_start_block="$i"
+		current_end_block=$((i + step_size - 1))
+		current_end_block=$((current_end_block > end_block ? end_block : current_end_block))
+
+		name="${NAME}-vm-${current_start_block}-${current_end_block}"
+		command=$(
+			cat <<- END
+				bash
+				source $ENVRC
+				time ./target/release/replay-vm \\
+					block-range $current_start_block $current_end_block mainnet
+			END
+		)
+		spawn "$name" "$command" && {
+			echo "Replaying block range $current_start_block-$current_end_block in session $name"
+	  }
+
+		name="${NAME}-native-${current_start_block}-${current_end_block}"
+		command=$(
+			cat <<- END
+				bash
+				source $ENVRC
+				time ./target/release/replay-native \\
+					block-range $current_start_block $current_end_block mainnet
+			END
+		)
+		spawn "$name" "$command" && {
+			echo "Replaying block range $current_start_block-$current_end_block in session $name"
+	  }
+	done
 }
 
 # Parse optional flags.
@@ -38,53 +90,15 @@ while getopts "hn:" opt; do
 done
 shift $((OPTIND - 1))
 
-# Read positional arguments.
-if [[ $# -lt 3 ]]; then
-	yell "expected 1 positional argument\n"
+# Read subcommand
+if [[ $# -lt 1 ]]; then
+	yell "expected subcommand\n"
 	usage
 fi
-START_BLOCK="$1"
-RANGE_SIZE="$2"
-N_WORKERS="$3"
+case "$1" in
+	range) ;;
+	*) yell "unknown subcommand" ;;
+esac
 
-step_size=$(((RANGE_SIZE + N_WORKERS - 1) / N_WORKERS))
-end_block=$((START_BLOCK + RANGE_SIZE - 1))
-
-echo "Building replay for Cairo Native"
-cargo build -q --release --features state_dump 2>/dev/null
-cp ./target/release/replay ./target/release/replay-native
-echo "Building replay for Cairo VM"
-cargo build -q --release --features state_dump,only_cairo_vm 2>/dev/null
-cp ./target/release/replay ./target/release/replay-vm
-
-for ((i = START_BLOCK ; i <= end_block ; i += step_size )); do
-	current_start_block="$i"
-	current_end_block=$((i + step_size - 1))
-	current_end_block=$((current_end_block > end_block ? end_block : current_end_block))
-
-	name="${NAME}-vm-${current_start_block}-${current_end_block}"
-	command=$(
-		cat <<- END
-			bash
-			source $ENVRC
-			time ./target/release/replay-vm \\
-				block-range $current_start_block $current_end_block mainnet
-		END
-	)
-	spawn "$name" "$command" && {
-		echo "Replaying block range $current_start_block-$current_end_block in session $name"
-  }
-
-	name="${NAME}-native-${current_start_block}-${current_end_block}"
-	command=$(
-		cat <<- END
-			bash
-			source $ENVRC
-			time ./target/release/replay-native \\
-				block-range $current_start_block $current_end_block mainnet
-		END
-	)
-	spawn "$name" "$command" && {
-		echo "Replaying block range $current_start_block-$current_end_block in session $name"
-  }
-done
+# Call subcommand
+"$@"
