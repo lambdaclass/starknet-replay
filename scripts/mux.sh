b79b6204a76ab638d906e0cfddd821888567cd77
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-10-14 13:12:40 -0300

    Add cross-platform support

--- scripts/mux.sh
+++ scripts/mux.sh
@@ -63,11 +63,13 @@ range() {
 	if [ "$SKIP" != "native" ]; then
 		echo "Building replay for Cairo Native"
 		cargo build --release --features structured_logging,state_dump 2>/dev/null
+		rm ./target/release/replay-native
 		mv ./target/release/replay ./target/release/replay-native
 	fi
 	if [ "$SKIP" != "vm" ]; then
 		echo "Building replay for Cairo VM"
 		cargo build --release --features structured_logging,state_dump,only_cairo_vm 2>/dev/null
+		rm ./target/release/replay-vm
 		mv ./target/release/replay ./target/release/replay-vm
 	fi
 
@@ -136,7 +138,7 @@ status() {
 				log="$line"
 				break
 			fi
-		done < <( echo "$logs" | tail -r ) >/dev/null 2>&1
+		done < <( echo "$logs" | tac ) >/dev/null 2>&1
 		if [ -z "${log:-}" ]; then
 			echo "Failed to find logs for session $name" >&2
 			continue
@@ -144,7 +146,14 @@ status() {
 
 		# Obtain duration by comparing last log timestamp, with initial timestmap.
 		timestamp=$(echo "$log" | jq -r .timestamp | sed -E "s/\.[0-9]+//")
-		timestamp_s=$(date -ujf "%Y-%m-%dT%H:%M:%SZ" "+%s" "$timestamp")
+		case $(uname) in
+			Darwin)
+				timestamp_s=$(date -ujf "%Y-%m-%dT%H:%M:%SZ" "+%s" "$timestamp")
+			;;
+			Linux)
+				timestamp_s=$(date -ud "$timestamp" "+%s")
+			;;
+		esac
 		duration_s=$(bc <<< "$timestamp_s-$init_time")
 		hours=$(bc <<< "$duration_s/3600")
 		minutes=$(bc <<< "($duration_s%3600)/60")
