189eeb7e19d78ed99c5fcf107dd29b6086037c76
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-10-14 10:12:19 -0300

    Add status command

--- scripts/mux.sh
+++ scripts/mux.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 ENVRC=.envrc
 
-yell() { echo -e "$0:" "$@" >&2; }
+yell() { echo -e "$0:" "$@"; }
 
 usage() {
-cat >&2 <<EOF
+cat <<EOF
 Usage: $0
 EOF
 exit 1
@@ -40,10 +40,10 @@ range() {
 	end_block=$((START_BLOCK + RANGE_SIZE - 1))
 
 	echo "Building replay for Cairo Native"
-	cargo build -q --release --features state_dump 2>/dev/null
+	cargo build -q --release --features structured_logging,state_dump 2>/dev/null
 	cp ./target/release/replay ./target/release/replay-native
 	echo "Building replay for Cairo VM"
-	cargo build -q --release --features state_dump,only_cairo_vm 2>/dev/null
+	cargo build -q --release --features structured_logging,state_dump,only_cairo_vm 2>/dev/null
 	cp ./target/release/replay ./target/release/replay-vm
 
 	for ((i = START_BLOCK ; i <= end_block ; i += step_size )); do
@@ -79,26 +79,62 @@ range() {
 	done
 }
 
-# Parse optional flags.
+status() {
+	{
+	echo -e "status\tname\tduration\tblock\tmessage"
+
+	tmux ls -F '#{session_id} #{session_name} #{session_created} #{pane_current_command}' |
+	while read -r _ name init_time command; do
+		if ! [[ $name == $NAME-* ]]; then
+	    continue;
+	  fi
+
+		if [[ $command == replay* ]]; then
+			status="RUNNING"
+		elif [[ $command == "bash" ]]; then
+			status="STOPPED"
+		else
+			status="$command"
+		fi
+
+		log=$(tmux capture-pane -pJt "$name" -S 0 -E 100)
+		last=$(echo "$log" | tail -n 1)
+
+		timestamp=$(echo "$last" | jq -r .timestamp 2>/dev/null | sed -E "s/\.[0-9]+//")
+		timestamp_s=$(date -jf "%Y-%m-%dT%H:%M:%SZ" "+%s" "$timestamp")
+		duration_s=$(bc <<< "$timestamp_s-$init_time")
+		hours=$(bc <<< "$duration_s/3600")
+		minutes=$(bc <<< "($duration_s%3600)/60")
+		seconds=$(bc <<< "$duration_s%60")
+		duration="$hours:$minutes:$seconds"
+
+		block=$(echo "$last" | jq '.spans[] | select (.name=="block execution") | .block' 2>/dev/null)
+		message=$(echo "$last" | jq .fields.message)
+
+		printf "%s\t%s\t%s\t%s\t%s\n" "$status" "$name" "$duration" "$block" "$message"
+	done
+	} |
+	column -t -s $'\t'
+}
+
+# Parse global optional flags.
 NAME="replay"
 while getopts "hn:" opt; do
 	case $opt in
 		h) usage ;;
 		n) NAME=$OPTARG ;;
-		*) echo >&2; usage ;;
+		*) echo; usage ;;
 	esac
 done
 shift $((OPTIND - 1))
 
-# Read subcommand
+# Call subcommand
 if [[ $# -lt 1 ]]; then
 	yell "expected subcommand\n"
 	usage
 fi
 case "$1" in
-	range) ;;
-	*) yell "unknown subcommand" ;;
+	range) range "${@:2}";;
+	status) status "${@:2}";;
+	*) yell "unknown subcommand: $1" ;;
 esac
-
-# Call subcommand
-"$@"
